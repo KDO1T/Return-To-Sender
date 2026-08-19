d4a52c0795d2a68e1f94cf2f1b1f0bef56601c19
Krem <karsean25@gmail.com>
2026-08-19 17:44:14 +0800

    First commit: testing out window:

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Return To Sender")
+import pygame, sys
+from pygame.locals import *
+pygame.init()
+
+clock = pygame.time.Clock()
+
+WINDOW_RESOLUTION = (400,400)
+screen = pygame.display.set_mode(WINDOW_RESOLUTION)
+
+while True:
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.update()
+    clock.tick(60)
