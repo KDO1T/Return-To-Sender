f85e66bc3331d8f137e4a0b1fc44593f5f63fd38
Pillow Turtle <dani.fadz@gmail.com>
2026-08-22 18:58:02 +0800

    added movement features

--- main.py
+++ main.py
@@ -9,17 +9,68 @@ screen = pygame.display.set_mode((0,0)) # (0,0) means it will fit whatever scree
 
 
 
+# *--PLAYER STUFF--*
+player_sprite = pygame.image.load('picture test.png')
+player_location = [50, 50]
+moving_up = False
+moving_down = False
+moving_right = False
+moving_left = False
+
 
 # *--GAME LOOP--*
 running = True
 while running: 
 
+    screen.blit(player_sprite, player_location)
+
+    if moving_up == True:
+        player_location[1] += 4 
+    if moving_down == True:
+        player_location[1] -= 4
+    if moving_right == True:
+        player_location[0] += 4
+    if moving_left == True:
+        player_location[0] -= 4
+
 
     # *--INPUT DETECTION--*
-    for event in pygame.event.get(): 
+
+    for event in pygame.event.get(): #event loop
+
+
+        # *--QUIT--*
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+
+        # *--KEY DETECTION--*
+
+            # *--KEY IS PRESSED--*
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w:#pressing W (up)
+                moving_up = True
+            if event.key == pygame.K_s:#pressing S (down)
+                moving_down = True
+            if event.key == pygame.K_d: #pressing D (right)
+                moving_right = True
+            if event.key == pygame.K_a: #pressing A (left)
+                moving_left = True
+
+            # *--KEY IS LET GO--*  
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_w:#let go of W (up)
+                moving_up = False
+            if event.key == pygame.K_s:#let go of S (down)
+                moving_down = False
+            if event.key == pygame.K_d: #let go of D (right)
+                moving_right = False
+            if event.key == pygame.K_a: #let go of A (left)
+                moving_left = False
+
+            
+
 
 
 
