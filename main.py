6a06dcd59e474a53f482e8ac8feae21052b35c84
Krem <karsean25@gmail.com>
2026-08-24 21:41:26 +0800

    Window Resolution + Window Sizing Got a functional scaling with user monitor resolution and baseline resolution, got a slightly functional window sizing mechanic except borderless, funky because of the top taskbar.

--- main.py
+++ main.py
@@ -6,10 +6,21 @@ pygame.init()
 
 clock = pygame.time.Clock()
 
-display_w , display_h = 768, 512
+#grab resolution for the users monitor
+resolution = pygame.display.get_desktop_sizes()
+#baseline resolution
+base_res_x, base_res_y = 640, 360        
 
-canvas = pygame.Surface((display_w,display_h))
-screen = pygame.display.set_mode((display_w,display_h))
+display_w , display_h = resolution[0]   #index 0 for the first monitor   
+window_w, window_h = 1280,720
+
+screen_state_w, screen_state_h = window_w, window_h
+
+#base window status
+status = RESIZABLE
+
+canvas = pygame.Surface((base_res_x, base_res_y))
+screen = pygame.display.set_mode((screen_state_w, screen_state_h), status)
 
 sprites = Spritesheet('spritesheet.png')
 
@@ -23,8 +34,35 @@ while True:
             pygame.quit()
             sys.exit()
 
+        if event.type == VIDEORESIZE and status == RESIZABLE:
+            window_w, window_h = event.w , event.h
+            screen_state_w, screen_state_h = window_w, window_h
+            screen = pygame.display.set_mode((screen_state_w, screen_state_h),status)
+
+        if event.type == KEYDOWN:
+            if event.key == K_F1:
+                status = RESIZABLE
+                screen_state_w, screen_state_h = window_w, window_h
+                screen = pygame.display.set_mode((screen_state_w, screen_state_h), status)
+
+            elif event.key == K_F2:
+                status = NOFRAME
+                screen_state_w, screen_state_h = display_w, display_h
+                screen = pygame.display.set_mode((display_w, display_h), status)
+
+            elif event.key == K_F3:
+                status = FULLSCREEN
+                screen_state_w, screen_state_h = display_w, display_h
+                screen = pygame.display.set_mode((display_w,display_h), status)
+                
+    
+
     canvas.fill((159, 215, 255))    #nice sky background
     map.draw_map(canvas)    #uses the draw_map function to blit the surface of the tilemap onto the screen
-    screen.blit(canvas,(0,0))   #creates a window to be displayed
+
+    #scale the screen
+    scaled_resolution = pygame.transform.scale(canvas, (screen_state_w, screen_state_h))
+
+    screen.blit(scaled_resolution,(0,0))   #creates a window to be displayed
     pygame.display.update()
     clock.tick(60)
