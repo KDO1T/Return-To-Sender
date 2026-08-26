fea15ffe4fd76470c0669e69675dc60e137cf3ad
KDO1T <khaled.mmoe@gmail.com>
2026-08-26 19:27:31 +0800

    added the play menu options (still static)

--- main_menu.py
+++ main_menu.py
@@ -13,42 +13,60 @@ pygame.display.set_caption("Return To Sender")
 
 clock = pygame.time.Clock()
 
-# --------------
-main_menu = ["Play", "Options", "Quit"]
-
-
 
 # fonts
 font_title = pygame.font.Font("fonts/Press_Start_2P/PressStart2P.ttf", 30)
 font_menu = pygame.font.Font("fonts/VT323/VT323.ttf", 45)
 
-option_text = font_menu.render("Play", False, (240, 240, 240))
+
 text = font_title.render("Return To Sender", False, (240, 240, 240))
 rect_text = text.get_rect()
 
 rect_text.center = (width/2, height/2)
 
-
+# text positioning
 start_y = 220
 spacing = 40
 
+play_start_y = 130
+play_spacing = 40
 
+# --------------
+main_menu = ["Play", "Options", "Quit"]
+play_menu = ["Save Slot 1", "Save Slot 2", "Save Slot 3", "Skill Tree", "Back"]
+
+play_selected = 0
 selected = 0
-
+current_state = "MAIN"
 
 while True:
     screen.fill((15, 15, 20))
-    for index, option in enumerate(main_menu):
-        y = start_y + (index * spacing)
 
-        if selected == index:
-            option_text =  font_menu.render(option, False, (235, 65, 40))
-        else:
-            option_text =  font_menu.render(option, False, (240, 240, 240))
+    if current_state == "MAIN":
+        for index, option in enumerate(main_menu):
+            y = start_y + (index * spacing)
 
-        option_rect = option_text.get_rect()
-        option_rect.center = (width/2, y)
-        screen.blit(option_text, option_rect)
+            if selected == index:
+                option_text =  font_menu.render(option, False, (235, 65, 40))
+            else:
+                option_text =  font_menu.render(option, False, (240, 240, 240))
+
+            option_rect = option_text.get_rect()
+            option_rect.center = (width/2, y)
+            screen.blit(option_text, option_rect)
+
+    elif current_state == "PLAY":
+        for index, option in enumerate(play_menu):
+            y = play_start_y + (index * play_spacing)
+
+            if play_selected == index:
+                option_text = font_menu.render(option, False, (235, 65, 40))
+            else:
+                option_text = font_menu.render(option, False, (240, 240, 240))
+
+            option_rect = option_text.get_rect()
+            option_rect.center = (width / 2, y)
+            screen.blit(option_text, option_rect)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -59,10 +77,13 @@ while True:
             if event.key == pygame.K_DOWN:
                 selected = (selected + 1) % len(main_menu)
 
-        elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_UP:
+            elif event.key == pygame.K_UP:
                 selected = (selected - 1) % len(main_menu)
 
+            elif event.key == pygame.K_RETURN:
+                if main_menu[selected] == "Play":
+                    current_state = "PLAY"
+                
     screen.blit(text, rect_text)
     pygame.display.update()
     clock.tick(60)
