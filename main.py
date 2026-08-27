7aff4ca245773b73a46e43aa8164af133036780b
Pillow Turtle <dani.fadz@gmail.com>
2026-08-28 01:38:43 +0800

    40% merged the player and map both load and from what i can tell the camera does follow the player but it is a little wonky. the code for collision is going to take a while so im going to attempt to fix that tomorrow.

--- main.py
+++ main.py
@@ -1,13 +1,44 @@
 import pygame, sys
+from pygame.locals import *
+from spritesheet import Spritesheet
+from tilemap import *
+pygame.init()
+
+#grab resolution for the users monitor
+resolution = pygame.display.get_desktop_sizes()
+#baseline resolution
+base_res_x, base_res_y = 640, 360        
+
+display_w , display_h = resolution[0]   #index 0 for the first monitor   
+window_w, window_h = 1280,720
+screen_state_w, screen_state_h = window_w, window_h
+
+#camera movement
+camera_x=0 #made it the same as the player's coordinates
+camera_y=0 #made it the same as the player's coordinates
+camera_speed=5 #will make this the difference in current player coordinates 
+
+#base window status
+status = RESIZABLE
 
 clock = pygame.time.Clock() #assigning the clock function to a variable to use for the fps in the gameloop
 
 #1. initiliaze pygame, 2. names the window, 3. sets the window size and sets its paramaters
 pygame.init()
 pygame.display.set_caption("Return To Sender") 
-window_size = (1000, 500)
-screen = pygame.display.set_mode(window_size) # fixed resolution, might change later due to technical issues
+canvas = pygame.Surface((base_res_x, base_res_y))
+screen = pygame.display.set_mode((screen_state_w, screen_state_h), status)
 
+# *-- MAP STUFF --*
+sprites = Spritesheet('spritesheet.png')
+map_grid = [['screenR1C1.csv', 'screenR1C2.csv','screenR1C3.csv'],
+            ['screenR2C1.csv','screenR2C2.csv','screenR2C3.csv']] #passes the csv file and the png file into the map variable
+
+maps = [[TileMap(file,sprites) for file in row] for row in map_grid]            #puts all row files in map_grid into a list, and putting all maps from the list into maps
+
+#combine both map (top and bottom)
+total_map_w = sum(tile_maps.map_w  for tile_maps in maps[0])   #loops through maps list
+total_map_h = sum(row[0].map_h for row in maps)
 
 
 # *--PLAYER STUFF--*
@@ -25,16 +56,16 @@ on_ground = None
 object_rect = pygame.Rect(300, 300, 250,250)
 
 # *--TILES OBJECTS--*
-floor_tiles = [pygame.Rect(0, window_size[1]-50, 200,50 ), pygame.Rect(200, window_size[1]-50, 200,50), 
-         pygame.Rect(400, window_size[1]-50, 200,50), pygame.Rect(600, window_size[1]-50, 200,50),
-         pygame.Rect(800, window_size[1]-50, 200,50 ),pygame.Rect(1000, window_size[1]-50, 200,50 ),
-         ]
+# floor_tiles = [pygame.Rect(0, window_size[1]-50, 200,50 ), pygame.Rect(200, window_size[1]-50, 200,50), 
+#          pygame.Rect(400, window_size[1]-50, 200,50), pygame.Rect(600, window_size[1]-50, 200,50),
+#          pygame.Rect(800, window_size[1]-50, 200,50 ),pygame.Rect(1000, window_size[1]-50, 200,50 ),
+#          ]
 
-platform_tiles = [
-                 pygame.Rect(0, window_size[1]-300, 200,50 ),  
-                pygame.Rect(400, window_size[1]-300, 200,50),
-                pygame.Rect(800, window_size[1]-300, 200,50 )
-                ]
+# platform_tiles = [
+#                  pygame.Rect(0, window_size[1]-300, 200,50 ),  
+#                 pygame.Rect(400, window_size[1]-300, 200,50),
+#                 pygame.Rect(800, window_size[1]-300, 200,50 )
+#                 ]
 
 
 
@@ -47,6 +78,10 @@ while running:
        # *--INPUT DETECTION--*
     for event in pygame.event.get(): #just detects if any 'events' occur
 
+        
+    
+
+
         # *--QUIT--*
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -54,7 +89,25 @@ while running:
 
         # *--KEY DETECTION--*
 
-            # *--KEY IS PRESSED--*
+            # *--WINDOW CONTROLS--*
+            if event.type == VIDEORESIZE and status == RESIZABLE:
+                                window_w, window_h = event.w , event.h
+                                screen_state_w, screen_state_h = window_w, window_h
+                                screen = pygame.display.set_mode((screen_state_w, screen_state_h),status)
+    
+            if event.type == KEYDOWN:
+                if event.key == K_F1:
+                    status = RESIZABLE
+                    screen_state_w, screen_state_h = window_w, window_h
+                    screen = pygame.display.set_mode((screen_state_w, screen_state_h), status)
+    
+                elif event.key == K_F11:
+                    status = FULLSCREEN
+                    screen_state_w, screen_state_h = display_w, display_h
+                    screen = pygame.display.set_mode((display_w,display_h), status)
+
+
+            # *--KEY PRESSED--*
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_w:#pressing W (up)
                 moving_up = True
@@ -91,14 +144,6 @@ while running:
                 moving_left = False
 
 
-
-
-
-
-
-
-
-
     # *--HORIZONTAL MOVEMENT + COLLISIONS--*
 
     player_movement = [0,0]  
@@ -113,14 +158,14 @@ while running:
         player_rect.x += player_movement[0]
  
     #collisions
-    for tile in platform_tiles:    
-        if player_rect.colliderect(tile):
+    # for tile in platform_tiles:    
+    #     if player_rect.colliderect(tile):
             
-            if player_movement[0] > 0:
-                player_rect.right = tile.left
+    #         if player_movement[0] > 0:
+    #             player_rect.right = tile.left
 
-            if player_movement[0] < 0:
-                player_rect.left = tile.right
+    #         if player_movement[0] < 0:
+    #             player_rect.left = tile.right
 
 
 
@@ -145,141 +190,55 @@ while running:
         player_y_momentum = -4.5
 
 
-    for tile in floor_tiles:
-        if player_rect.colliderect(tile):
-            if player_movement[1] > 0:
-                player_rect.bottom = tile.top
-                player_y_momentum = 0 # <-- basically tells the game that i can stop falling now
-                on_ground = True
+    # for tile in floor_tiles:
+    #     if player_rect.colliderect(tile):
+    #         if player_movement[1] > 0:
+    #             player_rect.bottom = tile.top
+    #             player_y_momentum = 0 # <-- basically tells the game that i can stop falling now
+    #             on_ground = True
         
-            if player_movement[1] < 0:
-                player_rect.top = tile.bottom
-                player_y_momentum = 0 # <-- same with this
+    #         if player_movement[1] < 0:
+    #             player_rect.top = tile.bottom
+    #             player_y_momentum = 0 # <-- same with this
 
 
 
-    for tile in platform_tiles:
-        if player_rect.colliderect(tile):
-            if player_movement[1] > 0:
-                player_rect.bottom = tile.top
-                player_y_momentum = 0 # <-- basically tells the game that i can stop falling now
-                on_ground = True
+    # for tile in platform_tiles:
+    #     if player_rect.colliderect(tile):
+    #         if player_movement[1] > 0:
+    #             player_rect.bottom = tile.top
+    #             player_y_momentum = 0 # <-- basically tells the game that i can stop falling now
+    #             on_ground = True
                 
-            if player_movement[1] < 0:
-                player_rect.top = tile.bottom
-                player_y_momentum = 0 # <-- same with this
+    #         if player_movement[1] < 0:
+    #             player_rect.top = tile.bottom
+    #             player_y_momentum = 0 # <-- same with this
 
     
 
     # *--RENDERING--*
-    
-
-    screen.fill((255,255,255)) #fills the background with white
-
-    # *--COLLISION TESTING--*
-    if player_rect.colliderect(object_rect):
-        pygame.draw.rect(screen, (255,0,0), object_rect)
-    else:
-        pygame.draw.rect(screen, (0, 255, 0), object_rect)
-
-    for tile in floor_tiles:
-        pygame.draw.rect(screen, (92, 64, 51), tile)
-
-    for tile in platform_tiles:
-        pygame.draw.rect(screen, (92, 64, 51), tile)
+ 
+    # *--CAMERA MOVEMENT--*
 
 
 
-    screen.blit(player_sprite, player_rect) #draws the player onto the location of its hitbox*
+    camera_x = player_rect.centerx - base_res_x // 2
+    camera_y = player_rect.centery - base_res_y // 2
 
-    pygame.display.update() #updates the screen
-    clock.tick(60) #ensures framerate is consistently 60fps
-
-
-
-#SEAN'S CODE
-from pygame.locals import *
-from spritesheet import Spritesheet
-from tilemap import *
-pygame.init()
-
-clock = pygame.time.Clock()
-
-#grab resolution for the users monitor
-resolution = pygame.display.get_desktop_sizes()
-#baseline resolution
-base_res_x, base_res_y = 640, 360        
-
-display_w , display_h = resolution[0]   #index 0 for the first monitor   
-window_w, window_h = 1280,720
-screen_state_w, screen_state_h = window_w, window_h
-
-#camera movement
-camera_x=0
-camera_y=0
-camera_speed=5
-
-#base window status
-status = RESIZABLE
-
-canvas = pygame.Surface((base_res_x, base_res_y))
-screen = pygame.display.set_mode((screen_state_w, screen_state_h), status)
-
-
-#map
-sprites = Spritesheet('spritesheet.png')
-map_grid = [['screenR1C1.csv', 'screenR1C2.csv','screenR1C3.csv'],
-            ['screenR2C1.csv','screenR2C2.csv','screenR2C3.csv']] #passes the csv file and the png file into the map variable
-
-maps = [[TileMap(file,sprites) for file in row] for row in map_grid]            #puts all row files in map_grid into a list, and putting all maps from the list into maps
-
-#combine both map (top and bottom)
-total_map_w = sum(tile_maps.map_w  for tile_maps in maps[0])   #loops through maps list
-total_map_h = sum(row[0].map_h for row in maps)
-
-while True:
-
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-
-        if event.type == VIDEORESIZE and status == RESIZABLE:
-            window_w, window_h = event.w , event.h
-            screen_state_w, screen_state_h = window_w, window_h
-            screen = pygame.display.set_mode((screen_state_w, screen_state_h),status)
-
-        if event.type == KEYDOWN:
-            if event.key == K_F1:
-                status = RESIZABLE
-                screen_state_w, screen_state_h = window_w, window_h
-                screen = pygame.display.set_mode((screen_state_w, screen_state_h), status)
-
-            elif event.key == K_F11:
-                status = FULLSCREEN
-                screen_state_w, screen_state_h = display_w, display_h
-                screen = pygame.display.set_mode((display_w,display_h), status)
-
-    #map movement ,+x,-x, +y,-y
-    key_input = pygame.key.get_pressed()
-    if key_input[K_a]:
-        camera_x -=camera_speed
-    if key_input[K_d]:
-        camera_x +=camera_speed
-    if key_input[K_w]:
-        camera_y -= camera_speed
-    if key_input[K_s]:
-        camera_y += camera_speed
-
-    #map clamping      
+     #map clamping      
     max_cam_x = total_map_w - base_res_x
     max_cam_y = total_map_h - base_res_y
 
     camera_x = max(0, min(camera_x, max_cam_x))
     camera_y = max(0, min(camera_y, max_cam_y))
 
+
+
+
+
     canvas.fill((159, 215, 255))    #nice sky background
     current_y= 0
+
     #filtering through the top and bottom layer in maps
     for row in maps:            
         current_x = 0
@@ -296,5 +255,133 @@ while True:
     scaled_resolution = pygame.transform.scale(canvas, (screen_state_w, screen_state_h))
 
     screen.blit(scaled_resolution,(0,0))   #creates a window to be displayed
-    pygame.display.update()
-    clock.tick(60)
+
+    screen.blit(player_sprite, player_rect) #draws the player onto the location of its hitbox*
+
+    pygame.display.update() #updates the screen
+    clock.tick(60) #ensures framerate is consistently 60fps
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# #SEAN'S CODE 
+# from pygame.locals import *
+# from spritesheet import Spritesheet
+# from tilemap import *
+# pygame.init()
+
+# clock = pygame.time.Clock()
+
+# #grab resolution for the users monitor
+# resolution = pygame.display.get_desktop_sizes()
+# #baseline resolution
+# base_res_x, base_res_y = 640, 360        
+
+# display_w , display_h = resolution[0]   #index 0 for the first monitor   
+# window_w, window_h = 1280,720
+# screen_state_w, screen_state_h = window_w, window_h
+
+# #camera movement
+# camera_x=0
+# camera_y=0
+# camera_speed=5
+
+# #base window status
+# status = RESIZABLE
+
+# canvas = pygame.Surface((base_res_x, base_res_y))
+# screen = pygame.display.set_mode((screen_state_w, screen_state_h), status)
+
+
+# #map
+# sprites = Spritesheet('spritesheet.png')
+# map_grid = [['screenR1C1.csv', 'screenR1C2.csv','screenR1C3.csv'],
+#             ['screenR2C1.csv','screenR2C2.csv','screenR2C3.csv']] #passes the csv file and the png file into the map variable
+
+# maps = [[TileMap(file,sprites) for file in row] for row in map_grid]            #puts all row files in map_grid into a list, and putting all maps from the list into maps
+
+# #combine both map (top and bottom)
+# total_map_w = sum(tile_maps.map_w  for tile_maps in maps[0])   #loops through maps list
+# total_map_h = sum(row[0].map_h for row in maps)
+
+# while True:
+
+#     for event in pygame.event.get():
+#         if event.type == QUIT:
+#             pygame.quit()
+#             sys.exit()
+
+#         if event.type == VIDEORESIZE and status == RESIZABLE:
+#             window_w, window_h = event.w , event.h
+#             screen_state_w, screen_state_h = window_w, window_h
+#             screen = pygame.display.set_mode((screen_state_w, screen_state_h),status)
+
+#         if event.type == KEYDOWN:
+#             if event.key == K_F1:
+#                 status = RESIZABLE
+#                 screen_state_w, screen_state_h = window_w, window_h
+#                 screen = pygame.display.set_mode((screen_state_w, screen_state_h), status)
+
+#             elif event.key == K_F11:
+#                 status = FULLSCREEN
+#                 screen_state_w, screen_state_h = display_w, display_h
+#                 screen = pygame.display.set_mode((display_w,display_h), status)
+
+#     #map movement ,+x,-x, +y,-y
+#     key_input = pygame.key.get_pressed()
+#     if key_input[K_a]:
+#         camera_x -=camera_speed
+#     if key_input[K_d]:
+#         camera_x +=camera_speed
+#     if key_input[K_w]:
+#         camera_y -= camera_speed
+#     if key_input[K_s]:
+#         camera_y += camera_speed
+
+#     #map clamping      
+#     max_cam_x = total_map_w - base_res_x
+#     max_cam_y = total_map_h - base_res_y
+
+#     camera_x = max(0, min(camera_x, max_cam_x))
+#     camera_y = max(0, min(camera_y, max_cam_y))
+
+#     canvas.fill((159, 215, 255))    #nice sky background
+#     current_y= 0
+#     #filtering through the top and bottom layer in maps
+#     for row in maps:            
+#         current_x = 0
+#         #filtering through each screen in each layer
+#         for tile_map in row:
+#             #drawing the map with respect to each offset
+#             tile_map.draw_map(canvas, camera_x, camera_y, offset_x = current_x, offset_y = current_y)
+#             #updating x offset
+#             current_x += tile_map.map_w
+#         #updating y offset
+#         current_y += row[0].map_h
+
+#     #scale the screen
+#     scaled_resolution = pygame.transform.scale(canvas, (screen_state_w, screen_state_h))
+
+#     screen.blit(scaled_resolution,(0,0))   #creates a window to be displayed
+#     pygame.display.update()
+#     clock.tick(60)
